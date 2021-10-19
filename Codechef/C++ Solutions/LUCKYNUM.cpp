9994682897d812f8c66d42e8779e704916393056
Ashutosh <70135665+Ashutosh-Ray@users.noreply.github.com>
2021-10-19 19:03:20 +0530

    Create LUCKYNUM.cpp

---
+++ Codechef/C++ Solutions/LUCKYNUM.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int test;
+    cin>>test;
+    while(test--)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a==7 || b==7 || c==7)
+        {
+            cout<<"YES";
+        }
+        else
+        {
+            cout<<"NO";
+        }
+        cout<<endl;
+
+    }
+    return 0;
+}
