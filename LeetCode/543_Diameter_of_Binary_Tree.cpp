965486fd05a3b4bd78f863224fd807d7b9547955
rohanlahori <64496391+rohanlahori@users.noreply.github.com>
2021-10-27 10:45:44 +0530

    Create 543_Diameter_of_Binary_Tree.cpp

---
+++ LeetCode/543_Diameter_of_Binary_Tree.cpp
@@ -0,0 +1,60 @@
+Problem Link:
+
+https://leetcode.com/problems/diameter-of-binary-tree/
+
+My Submission:
+
+https://leetcode.com/submissions/detail/554266228/
+
+My Discuss Post
+(Refer for detailed Explaination)
+  https://leetcode.com/problems/diameter-of-binary-tree/discuss/1461590/traversing-all-subtree-depth-cpp-soultion
+
+Code:
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+int height_of_subtree(TreeNode* node)
+{
+    if(node==NULL)
+    {
+        return 0;
+    }
+    return 1+max(height_of_subtree(node->left),height_of_subtree(node->right));
+}
+void preorder(TreeNode* root,int &mx)
+{
+    int height=0;
+    if(root==NULL)
+    {
+        return;
+    }
+    if(root->left!=NULL)
+    {
+        height+=height_of_subtree(root->left);
+    }
+    if(root->right!=NULL)
+    {
+        height+=height_of_subtree(root->right);
+    }
+    mx=max(mx,height);
+    preorder(root->left,mx);
+    preorder(root->right,mx);
+}
+
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        int max_height=0;
+        preorder(root,max_height);
+        return max_height;
+    }
+};
